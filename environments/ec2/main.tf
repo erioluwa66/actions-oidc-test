7e16ce160b0fe49442353a236e35be0faba43813
erioluwa66 <erioluwa66@gmail.com>
2023-10-02 15:38:20 -0400

    vpc modigy

--- environments/ec2/main.tf
+++ environments/ec2/main.tf
@@ -2,8 +2,24 @@ module "ec2_instance" {
   source = "terraform-aws-modules/ec2-instance/aws"
 
   name          = "var.name"
-  instance_type = "var.instance_type"
+  instance_type = "t2.micro"
   monitoring    = true
-  subnet_id     = var.subnet_id
+
+  data "terraform_remote_state" "vpc" {
+    backend = "remote"
+
+    config = {
+      organization = "hashicorp"
+      workspaces = {
+        name = "vpc-demo"
+      }
+    }
+  }
+
+  # Terraform >= 0.12
+  resource "aws_instance" "ec2-demo" {
+    # ...
+    subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
+  }
 
 }
--- environments/ec2/variables.tf
+++ environments/ec2/variables.tf
@@ -4,13 +4,6 @@ variable "name" {
   default     = "ec2-demo"
 
 }
-
-variable "instance_type" {
-  type        = string
-  description = "Project mariam"
-  default     = "t2.micro"
-
-}
 variable "subnet_id" {
   type    = string
   default = "subnet-07bf2b750819e534b"
